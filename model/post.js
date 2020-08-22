f6ae737cb6951d5a3a21516b74091185acd8f731
parinaz <parinazmohamadianraouf@yahoo.com>
2020-08-22 17:55:37 +0430

    Editet mongoose models

--- model/post.js
+++ model/post.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('~/database');
+// const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postsSchema = new Schema({
